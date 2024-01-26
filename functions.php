1e1c27cdb8853ffbc8499fc7b531e74470000314
chiara.ruggi <chiara.ruggi1998@gmail.com>
2024-01-26 12:14:53 +0100

    changed name on functions.php

--- functions.php
+++ functions.php
@@ -4,9 +4,9 @@ function getRandomPassword()
     $password = $_GET["password"];
     $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!.,$%&/()&/';
     $n = [];
-    $charactersLength = strlen($characters) - 1;
+    $Length = strlen($characters) - 1;
     for ($i = 0; $i < $password; $i++) {
-        $num = rand(1, $charactersLength);
+        $num = rand(1, $Length);
         $n[] = $characters[$num];
     }
     return implode($n);
